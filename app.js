84f8fb564aca873317091a96bdf426b03d4f132c
deepu-raj <deepuraj.devan@gmail.com>
2021-08-22 15:02:22 +0530

    Final Commit : fixed errors and done.

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var inputEmoji = document.querySelector("input");
+var inputEmoji = document.querySelector('input');
 console.log(inputEmoji);
-var outputText = document.getElementById("translationOutput");
+var outputText = document.getElementById('translationOutput');
 const emojiDictionary = {
     "😀": "Happy or Smiling",
     "😉": "Winking",
     "🤩": "Star struck or super excited",
     "😬": "Grimacing",
     "😓": "Sad sweat face",
-    "😳": "Blushing",
+    "😳": "Blushing"
   };
 function translation (e) {
     var temp1 = e.target.value;
         if (emojiDictionary[temp1]=== undefined) {
-            temp1="This does not exist in out database;"
+            temp1="This does not exist in our database"
             } else {
                 temp1= emojiDictionary[temp1];
                 }
-    outputText.value = temp1;
+    outputText.innerHTML = temp1;
 }
-inputEmoji.addEventListener('change', translation);
+inputEmoji.addEventListener('input', translation);
 
